f7cb6a9f5f5a23b7d6276b2964b99fc4d3db1359
shellym96 Maughan <shellymaughan10@gmail.com>
2024-01-03 14:02:44 +0000

    Remove ID from AWS secret access key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -192,7 +192,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-online1'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = F'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
